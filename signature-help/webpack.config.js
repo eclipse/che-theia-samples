05e8b46f253e5729c3bd0fd016eea774a0f17e7e
Mykola Morhun <mmorhun@redhat.com>
2018-10-01 13:00:12 +0300

    Add signature help sample plugin (#5)
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

---
+++ signature-help/webpack.config.js
@@ -0,0 +1,48 @@
+/*********************************************************************
+* Copyright (c) 2018 Red Hat, Inc.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/signature-help-frontend-plugin.ts',
+    devtool: 'source-map',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    }
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'])
+    ],
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'signature-help-frontend-plugin.js',
+
+        libraryTarget: "var",
+        library: "theia_signature_help_plugin",
+
+        path: path.resolve(__dirname, 'dist')
+    },
+    externals: {
+            "@theia/plugin": "theia.theia_signature_help_plugin"
+	}
+};
