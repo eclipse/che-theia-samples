bd6d44e90f738e6e742f5e7fb3f2ce0d1a24ce8b
Mykola Morhun <mmorhun@redhat.com>
2018-10-19 16:55:46 +0300

    Fix build of formatter plugin (#11)
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- formatter-providers-plugin/src/formatter-providers-frontend-plugin.ts
+++ formatter-providers-plugin/src/formatter-providers-frontend-plugin.ts
@@ -15,7 +15,7 @@ export function start(context: theia.PluginContext) {
      *
      * Open the Command Palette (press F1 or Ctrl+Shift+P) and run command 'Format Document'
      */
-    const documentSelector = {scheme: 'file'};
+    const documentSelector = { scheme: 'file' };
     const documentFormattingProvider = {
         provideDocumentFormattingEdits: (document: theia.TextDocument, options: theia.FormattingOptions) => {
             const edits: theia.TextEdit[] = [];
@@ -43,7 +43,7 @@ export function start(context: theia.PluginContext) {
      * Select lines, open the Command Palette (press F1 or Ctrl+Shift+P)
      * and run command 'Format Selection'
      */
-    const rangeDocumentSelector = {scheme: 'file'};
+    const rangeDocumentSelector = { scheme: 'file' };
     const rangeFormattingProvider = {
         provideDocumentRangeFormattingEdits: (document: theia.TextDocument, range: theia.Range, options: theia.FormattingOptions) => {
             let lineNumber = range.start.line;
@@ -75,7 +75,7 @@ export function start(context: theia.PluginContext) {
      * Start typing and press ';' symbol or hit 'Enter' for a new line.
      * The '+' symbol should appear at the beginning of the line.
      */
-    const onTypeDocumentSelector = {scheme: 'file'};
+    const onTypeDocumentSelector = { scheme: 'file' };
     const onTypeFormattingProvider = {
         provideOnTypeFormattingEdits: (document: theia.TextDocument, position: theia.Position, ch: string, options: theia.FormattingOptions) => {
             const edit = new theia.TextEdit(
