4782aa76c2395349ad8246e3060e1a13275eb973
Vitaliy Gulyy <vgulyy@redhat.com>
2018-10-26 16:58:50 +0300

    Tree View sample plugin

--- tree-view-sample-plugin/src/comments.ts
+++ tree-view-sample-plugin/src/comments.ts
@@ -6,100 +6,109 @@ comments.set('Ann', ['Great plugin!', 'Can you share the link to this example?']
 comments.set('Yevhen', ['Cleanup your code please']);
 comments.set('Sun', ['Do not forget about docs']);
 
-const onDidSelectUserCommand: theia.Command = {
-    id: 'comments-tree-on-did-select-user',
-    label: 'On did select user'
-};
-
-const onDidSelectCommentCommand: theia.Command = {
-    id: 'comments-tree-on-did-select-comment',
-    label: 'On did select comment'
-};
-
-let treeDataProvider: TestDataProvider;
+const ON_DID_SELECT_USER = 'comments-tree-on-did-select-user';
+const ON_DID_SELECT_COMMENT = 'comments-tree-on-did-select-comment';
 
 export class Comments {
 
-    constructor(public disposables: theia.Disposable[]) {
-        treeDataProvider = new TestDataProvider();
-        const tree = theia.window.createTreeView('comments', { treeDataProvider });
+    treeDataProvider: TestDataProvider;
+    tree: theia.TreeView<string>;
 
-        tree.onDidExpandElement(event => {
+    selectedUser: string | undefined = undefined;
+
+    constructor(public disposables: theia.Disposable[]) {
+        this.treeDataProvider = new TestDataProvider();
+        this.tree = theia.window.createTreeView('comments', { treeDataProvider: this.treeDataProvider });
+
+        this.tree.onDidExpandElement(event => {
             // handle expanding
         });
 
-        tree.onDidCollapseElement(event => {
+        this.tree.onDidCollapseElement(event => {
             // handle collapsing
         });
 
         theia.commands.registerCommand({
             id: 'tree-view-add-user',
             label: '[TreeView] Add User'
-        }, addUser);
+        }, args => this.addUser(args));
 
         theia.commands.registerCommand({
             id: 'tree-view-add-comment',
             label: '[TreeView] Add Comment'
-        }, addComment);
+        }, args => this.addComment(args));
 
-        theia.commands.registerCommand(onDidSelectUserCommand, onDidSelectUser);
-        theia.commands.registerCommand(onDidSelectCommentCommand, onDidSelectComment);
+        theia.commands.registerCommand({
+            id: ON_DID_SELECT_USER,
+            label: 'On did select user'
+        }, args => this.onDidSelectUser(args));
+
+        theia.commands.registerCommand({
+            id: ON_DID_SELECT_COMMENT,
+            label: 'On did select comment'
+        }, args => this.onDidSelectComment(args));
     }
 
-}
-
-let selectedUser: string | undefined = undefined;
-
-export function onDidSelectUser(...args: any[]) {
-    if (args && args.length > 0) {
-        selectedUser = args[0].toString();
-    } else {
-        selectedUser = undefined;
-    }
-}
-
-export function onDidSelectComment(...args: any[]) {
-    selectedUser = undefined;
-
-    if (args && args.length > 0) {
-        let selectedComment = args[0].toString();
-        comments.forEach((comments, user) => {
-            comments.forEach(comment => {
-                if (selectedComment === comment) {
-                    selectedUser = user;
-                }
-            });
-        });
-    }
-}
-
-export function addUser(...args: any[]) {
-    theia.window.showInputBox({
-        prompt: 'Type user name to be added to the list',
-        placeHolder: 'User name'
-    }).then(value => {
-        if (value) {
-            comments.set(value, []);
-            treeDataProvider.sendDataChanged();
+    onDidSelectUser(...args: any[]) {
+        if (args && args.length > 0) {
+            this.selectedUser = args[0].toString();
+        } else {
+            this.selectedUser = undefined;
         }
-    });
-}
+    }
 
-export function addComment(...args: any[]) {
-    if (selectedUser) {
+    onDidSelectComment(...args: any[]) {
+        this.selectedUser = undefined;
+
+        if (args && args.length > 0) {
+            let selectedComment = args[0].toString();
+            comments.forEach((comments, user) => {
+                comments.forEach(comment => {
+                    if (selectedComment === comment) {
+                        this.selectedUser = user;
+                    }
+                });
+            });
+        }
+    }
+
+    addUser(...args: any[]) {
         theia.window.showInputBox({
-            prompt: 'Type comment added to user',
-            placeHolder: 'Comment'
+            prompt: 'Type user name to be added to the list',
+            placeHolder: 'User name'
         }).then(value => {
             if (value) {
-                const commentsArray: string[] | undefined = comments.get(selectedUser!);
-                if (commentsArray) {
-                    commentsArray.push(value);
-                    treeDataProvider.sendDataChanged();
-                }
+                comments.set(value, []);
+                this.treeDataProvider.sendDataChanged();
+
+                setTimeout(() => {
+                    this.tree.reveal(value);
+                }, 100);
             }
         });
     }
+
+    addComment(...args: any[]) {
+        if (this.selectedUser) {
+            theia.window.showInputBox({
+                prompt: 'Type comment added to user',
+                placeHolder: 'Comment'
+            }).then(value => {
+                if (value) {
+                    const commentsArray: string[] | undefined = comments.get(this.selectedUser!);
+                    if (commentsArray) {
+                        commentsArray.push(value);
+                        this.treeDataProvider.sendDataChanged();
+
+                        setTimeout(() => {
+                            this.tree.reveal(value);
+                        }, 100);
+                    }
+                }
+            });
+        }
+    }
+
 }
 
 export class TestDataProvider implements theia.TreeDataProvider<string> {
@@ -123,7 +132,7 @@ export class TestDataProvider implements theia.TreeDataProvider<string> {
                 label: element,
                 iconPath: 'fa-user medium-yellow',
                 command: {
-                    id: onDidSelectUserCommand.id,
+                    id: ON_DID_SELECT_USER,
                     arguments: [element]
                 },
 
@@ -135,10 +144,9 @@ export class TestDataProvider implements theia.TreeDataProvider<string> {
             label: element,
             iconPath: 'fa-sticky-note medium-red',
             command: {
-                id: onDidSelectCommentCommand.id,
+                id: ON_DID_SELECT_COMMENT,
                 arguments: [element]
-            },
-
+            }
         });
     }
 
