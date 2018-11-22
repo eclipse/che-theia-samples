c7465807bb6e59fff591c06bed60611d2f70f2b2
Mykola Morhun <mmorhun@redhat.com>
2018-11-22 12:53:25 +0200

    Add filesystem watchers sample (#18)

---
+++ filesystem-watchers/README.md
@@ -0,0 +1,8 @@
+## filesystem-watchers plugin
+
+Simple Theia plugin to demonstrate file system watcher plugin API.
+
+Contains examples of:
+ - tracking of C++ source files inside all `src` directories
+ - tracking creation / deletion of `package.json` files in specific workspace root folder
+ - tracking changes in any typescript or javascript file
---
+++ filesystem-watchers/webpack.config.js
@@ -0,0 +1,46 @@
+/*********************************************************************
+* Copyright (c) 2018 Red Hat, Inc.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/filesystem-watchers-frontend.ts',
+    devtool: 'source-map',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    }
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'])
+    ],
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'filesystem-watchers-frontend.js',
+        libraryTarget: "var",
+        library: "theia_filesystem_watchers",
+        path: path.resolve(__dirname, 'dist')
+    },
+    externals: {
+        "@theia/plugin": "theia.theia_filesystem_watchers"
+	}
+};
