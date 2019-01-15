ffb35d40cb3b18e5e8f4281cf6d8ff51c2689640
Oleksandr Andriienko <oandriie@redhat.com>
2019-01-15 10:16:22 +0000

    Move code to the samples folder.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- samples/terminal-plugin/src/new-terminal-backend.ts
+++ samples/terminal-plugin/src/new-terminal-backend.ts
@@ -7,51 +7,42 @@ import * as theia from '@theia/plugin';
 
 export namespace Commands {
     export const CreateTerminalWithHelpArgs: theia.Command = {
-        id: "backend-plugin-terminal-created-with-help-args",
+        id: "terminal-created-with-help-args",
         label: "Create terminal with help arguments Server plugin"
     }
 
     export const CreateTerminalWithOptions: theia.Command = {
-        id: "backend-plugin-terminal-created-with-help-options",
+        id: "terminal-created-with-help-options",
         label: "Create terminal with help options Server plugin"
     }
 
     export const SendTextToTheTerminal: theia.Command = {
-        id: "backend-plugin-send-text-to-the-terminal",
+        id: "send-text-to-the-terminal",
         label: "Send text to the terminal Server plugin"
     }
 
     export const HidePanelWithTerminal: theia.Command = {
-        id: "backend-plugin-hide-terminal-panel",
+        id: "hide-terminal-panel",
         label: "Hide terminal panel after 3 sec Server plugin"
     }
 
     export const ShowTerminalWithDelay: theia.Command = {
-        id: "backend-plugin-show-terminal-with-delay",
+        id: "show-terminal-with-delay",
         label: "Show terminal after 3 sec Server plugin"
     }
 
     export const DisposeTerminal: theia.Command = {
-        id: "backend-plugin-dispose-terminal",
+        id: "dispose-terminal",
         label: "Dispose terminal after 3 sec Server plugin"
     }
 
     export const SubscribeToOnDidCloseTerminalEvent: theia.Command = {
-        id: "backend-plugin-subscribe-on-did-close-terminal-event",
+        id: "subscribe-on-did-close-terminal-event",
         label: "Subscribe to onDidCloseTerminal event Server plugin"
     }
 }
 
 export function start(context: theia.PluginContext) {
-    const informationMessageTestCommand = {
-        id: 'hello-world-example-generated',
-        label: "Hello World"
-    };
-    context.subscriptions.push(theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
-        theia.window.showInformationMessage('Hello World!');
-    }));
-
-
     context.subscriptions.push(theia.commands.registerCommand(Commands.CreateTerminalWithHelpArgs, () => {
         const terminal = theia.window.createTerminal("Sh Terminal", "sh", ["-l"]);
         terminal.show();
