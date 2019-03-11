f2c638a280a551ced71099f5f803b4b815f2311e
Artem Zatsarynnyi <azatsary@redhat.com>
2019-03-11 12:18:10 +0200

    Add copyright headers
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- samples/fs-plugin/src/fs-plugin.ts
+++ samples/fs-plugin/src/fs-plugin.ts
@@ -1,6 +1,14 @@
-/**
- * Plugin demonstrates using the `workspace.registerFileSystem` API.
- */
+/*********************************************************************
+* Copyright (c) 2018 Red Hat, Inc.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+
+/** Plugin demonstrates using the `workspace.registerFileSystem` API. */
 import * as theia from '@theia/plugin';
 import { DemoFileSystemProvider } from './demo-fs-provider';
 
